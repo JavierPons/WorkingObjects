24c27b0a2e6fa59fbfe1f51b6a69871543c911f0
Javier Pons <jpescandell@hotmail.com>
2018-07-07 23:26:40 +0300

    First Work

---
+++ object.js
@@ -0,0 +1,43 @@
+// Code goes here
+
+var todosList = {
+  todos: [],
+  displayTodos: function(){
+    if(this.todos.length === 0) {
+      console.log('Your array is empty');
+    } else {
+    
+    console.log('My todos are: ');
+    for(var i = 0; i < this.todos.length; i++){
+     
+      if(this.todos[i].completed === true){
+         console.log('(X)',this.todos[i].todoText);
+      } else {
+         console.log('( )',this.todos[i].todoText);
+      }
+    }
+  }
+  
+},
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodos: function(position, todoText){
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodos: function(position){
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position){
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+  
+}
